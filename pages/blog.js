1cce270ef940a3fb503e062a099ae536a17d4b48
jacarina <gtsbyxx@gmail.com>
2023-02-02 03:33:20 +0000

    Fix 2

--- pages/blog.js
+++ pages/blog.js
@@ -1,39 +1,39 @@
-import Layout from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts";
-import Link from "next/link";
-
-import React from "react";
-
-// Blog page
-export default function Blog({ allPostsData }) {
-  return (
-    <Layout>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <div className={utilStyles.lightText}>just noise</div>
-        <ul className={`${utilStyles.list} ${utilStyles.spacing}`}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/blog/${id}`}>
-                <div>{title}</div>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>"date"</small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  );
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
+// import Layout from "../components/layout";
+// import utilStyles from "../styles/utils.module.css";
+// import { getSortedPostsData } from "../lib/posts";
+// import Link from "next/link";
+//
+// import React from "react";
+//
+// // Blog page
+// export default function Blog({ allPostsData }) {
+//   return (
+//     <Layout>
+//       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+//         <h2 className={utilStyles.headingLg}>Blog</h2>
+//         <div className={utilStyles.lightText}>justnoise</div>
+//         <ul className={`${utilStyles.list} ${utilStyles.spacing}`}>
+//           {allPostsData.map(({ id, date, title }) => (
+//             <li className={utilStyles.listItem} key={id}>
+//               <Link href={`/blog/${id}`}>
+//                 <div>{title}</div>
+//               </Link>
+//               <br />
+//               <small className={utilStyles.lightText}>"date"</small>
+//             </li>
+//           ))}
+//         </ul>
+//       </section>
+//     </Layout>
+//   );
+// }
+//
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData();
+//
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
