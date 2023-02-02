82762a4caf45436edbda2bdaa9e1af3da4e2b203
jacarina <gtsbyxx@gmail.com>
2023-02-02 04:06:16 +0000

    test

--- pages/blog.js
+++ pages/blog.js
@@ -11,8 +11,18 @@ export default function Blog({ allPostsData }) {
     <Layout>
       <section className={utilStyles.headingMd}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
-        <div className={utilStyles.lightText}>justnoise</div>
-        <ul className={utilStyles.list}></ul>
+        <div className={utilStyles.lightText}>just noise</div>
+        <ul className={`${utilStyles.list} ${utilStyles.spacing}`}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/blog/${id}`}>
+                <div>{title}</div>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>"date"</small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
